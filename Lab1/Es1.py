d33903773fedcfe8f16320693d0f37ee21aefe69
gabrjc <gabrjc@gmail.com>
2022-10-16 21:32:18 +0200

    final

--- Lab1/Es1.py
+++ Lab1/Es1.py
@@ -1,5 +1,4 @@
-
-
+import logging
 import random
 
 def problem(N, seed=None):
@@ -9,13 +8,8 @@ def problem(N, seed=None):
         for n in range(random.randint(N, N * 5))
     ]
 
-
-
-
-import logging
-
-
 def greedy(N,l):
+    #made by Assignement
     goal = set(range(N))
     covered = set()
     solution = list()
@@ -31,86 +25,79 @@ def greedy(N,l):
         f"Greedy solution for N={N}: w={sum(len(_) for _ in solution)} (bloat={(sum(len(_) for _ in solution)-N)/N*100:.0f}%)"
     )
     logging.debug(f"{solution}")
+    print("")
 
 
-def longest_first(N,l):
+
+def subset_len(N, sets):
+
+
+
+    goal = set(range(0, N))
+    all_list_sets = [set(x) for x in sets]
+
     
-    goal = set(range(N))
-    covered = set()
-    solution = list()
-    all_lists = sorted(l, key=lambda l: -len(l))
-    while goal != covered:
-        x = all_lists.pop(0)
-        if not set(x) < covered:
-            solution.append(x)
-            covered |= set(x)
+    all = set()
+    for s in all_list_sets:
+            for e in s:
+                all.add(e)
+    
+    if all != goal:        
+        print("Problem not resolvable")
+        return None
 
-    #print("Bigger First : ", solution)
+    covered = set()
+    solution = list()  
+    while covered != goal:
+        subset = max(all_list_sets, key=lambda s: len(s - covered)/len(s))
+        solution.append(subset)
+        covered |= subset
+    
     logging.info(
-        f"Longest first solution for N={N}: w={sum(len(_) for _ in solution)} (bloat={(sum(len(_) for _ in solution)-N)/N*100:.0f}%)"
+        f"Subset_len solution for N={N}: w={sum(len(_) for _ in solution)} (bloat={(sum(len(_) for _ in solution)-N)/N*100:.0f}%)"
     )
     logging.debug(f"{solution}")
-    
+    print("")
 
-def mixed(N,l):
+
+def len_intersection_ratio(N, sets):
+
+    goal = set(range(0, N))
+    all_list_sets = [set(x) for x in sets]
+    all = set()
+
+    for s in all_list_sets:
+            for e in s:
+                all.add(e)
     
-    goal = set(range(N))
+    if all != goal:
+        print("Problem not resolvable")
+        return None
+
     covered = set()
     solution = list()
-    all_lists = sorted(l, key=lambda l: len(l))
-
-    while goal != covered:
-        if(covered.__len__()/goal.__len__()<0.70):
-            x = all_lists.pop(0)
-        else:
-            x= all_lists.pop()
-        
-        if not set(x) < covered:
-            solution.append(x)
-            covered |= set(x)
-
-    #print("Bigger First : ", solution)
-    logging.info(
-        f"Mixed solution for N={N}: w={sum(len(_) for _ in solution)} (bloat={(sum(len(_) for _ in solution)-N)/N*100:.0f}%)"
-    )
-    logging.debug(f"{solution}")
-
-
-def alternate(N,l):
+    while covered != goal:
+        subset = max(all_list_sets, key=lambda s: len(s - covered) / (len(s.intersection(covered))+1))
+        solution.append(subset)
+        covered |= subset
     
-    goal = set(range(N))
-    covered = set()
-    solution = list()
-    all_lists = sorted(l, key=lambda l: len(l))
-    alt=0
-    while goal != covered:
-        if(alt==0):
-            x = all_lists.pop(0)
-            alt=1
-        else:
-            x= all_lists.pop()
-            alt=0
-        
-        if not set(x) < covered:
-            solution.append(x)
-            covered |= set(x)
     logging.info(
-        f"Alternate solution for N={N}: w={sum(len(_) for _ in solution)} (bloat={(sum(len(_) for _ in solution)-N)/N*100:.0f}%)"
+        f"Len_intersection ratio solution for N={N}: w={sum(len(_) for _ in solution)} (bloat={(sum(len(_) for _ in solution)-N)/N*100:.0f}%)"
     )
     logging.debug(f"{solution}")
-
-
-
+    print("")
 
 
 if __name__=="__main__":
     logging.getLogger().setLevel(logging.INFO)
+    A=[5, 10, 20, 100, 500, 1000, 2500, 5000]
+    B=[10]
     
-    for N in [5, 10, 20, 100, 500, 1000]:
+    for N in A:
+        
         l = problem(N,42)
         greedy(N,l)
-        longest_first(N,l)
-        mixed(N,l)
-        alternate(N,l)
+        subset_len(N,l)
+        len_intersection_ratio(N,l)
         print("")
         print("")
